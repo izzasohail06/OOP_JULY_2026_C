9ca81c4e4348c62fdf4b070c6a1af467c738ed40
izzasohail06 <izzasohail06@gmail.com>
2026-08-26 22:03:29 +0500

    week 05

--- Employees/Date.java
+++ Employees/Date.java
@@ -3,12 +3,14 @@ class Date{
     private int date;
     private int month;
 
+    //null constructor
     Date(){
         this.date = 1;
         this.year = 2000;
         this.month = 2;
     }
 
+    //parameterized constructor
     Date(int date, int month, int year){
         if (date <= 31 && month <= 12 && year < 2026){
             this.date = date;
@@ -17,12 +19,55 @@ class Date{
         }
     }
 
+    //copy constructor
     Date(Date d){
         this.date = d.date;
         this.year = d.year;
         this.month = d.month;
     }
 
+    //Setters
+    public void set(int date, int month, int year){
+        this.date = date;
+        this.year = year;
+        this.month = month;
+    }
+
+    //Getters
+    public int getDate(){
+        return this.date;
+    }
+    public int getMonth(){
+        return this.month;
+    }
+    public int getYear(){
+        return this.year;
+    }
+
+    //Date Validation
+    public boolean isValidDate(int date, int month, int year) {
+        if (this.year < 1 || this.month < 1 || this.month > 12 || this.date < 1) {
+            return false;
+        }
+        return this.date;
+    }
+
+    //Days in Month
+    public int daysInMonth(int date, int month, int year){
+        if(this.month == 2){
+            if(this.year % 4 ==0){
+                return 29;
+            }else{
+                return 28;
+            }
+        }
+        if (this.month == 4 || this.month == 6 || this.month == 9 || this.month == 11){
+            return 30;
+        }else{
+            return 31;
+        }
+    }
+    //comparing dates
     public boolean compare(Date other){
         if(this.year == other.year && other == null && this.month == other.month && this.date == other.date){
             return false;
@@ -32,6 +77,7 @@ class Date{
         }
     }
     
+    //equal
     public boolean equals(Date other){
         if(this.year == other.year && other == null){
             return false;
@@ -40,6 +86,8 @@ class Date{
             return true;
         }
     }
+
+    //tostring
     public String toString(){
         return "" + this.date + "-" + this.month + "-" + this.year + "" ;
     }
--- Employees/Main.java
+++ Employees/Main.java
@@ -1,9 +1,16 @@
 public class Main{
     public static void main(String[] args) {
-        Date dob = new Date(26,4,2008);
-        Date doh = new Date(14,8,2025);
+        Date dob = new Date(20, 4, 2008);
+        Date doh = new Date(14, 8, 2025);
+
+        Date dob1 = new Date(20, 4, 2008);
+        Date doh1 = new Date(14, 8, 2025);
+
         Employee Amir = new Employee("Amir", dob, doh);
-        System.out.println(Amir);
+        Employee Izza = new Employee("Izza", dob1, doh1);
+        Amir.increment(Izza);
+        Amir.display();
+        Izza.display();
 
     }
 }
