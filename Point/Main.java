8bd271cae292fcc72968cfdbd9b344d62f854b6f
izzasohail06 <izzasohail06@gmail.com>
2026-09-03 02:01:54 +0500

    week 02

--- Point/Main.java
+++ Point/Main.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class Main{
     public static void main(String[] args){
         Point p1 = new Point(10,10);
--- Point/Point.java
+++ Point/Point.java
@@ -3,61 +3,65 @@ public class Point {
     private double y;
     private double distance;
 
-    Point(){
+    Point() {
         this.x = 3;
         this.y = 3;
     }
 
-    Point(double x, double  y){
+    Point(double x, double y) {
         this.x = x;
         this.y = y;
 
     }
-    
-    Point(Point p){
+
+    Point(Point p) {
         this.x = p.x;
         this.y = p.y;
     }
 
-    public void set(double x, double y){
+    public void set(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    
-    public double get_x(){
+
+    public double get_x() {
         return this.x;
     }
-    public double get_y(){
+
+    public double get_y() {
         return this.y;
     }
 
-    void add(Point n){
-         this.x += n.x;
-         this.y += n.y;
+    public void add(Point n) {
+        this.x += n.x;
+        this.y += n.y;
     }
-    void add(Point n , Point m ){
+
+    public void add(Point n, Point m) {
         this.x = n.x + m.x;
         this.y = n.y + m.y;
     }
 
-    void sub(Point n){
-         this.x -= n.x;
-         this.y -= n.y;
+    public void sub(Point n) {
+        this.x -= n.x;
+        this.y -= n.y;
     }
-    void sub(Point n , Point m ){
+
+    public void sub(Point n, Point m) {
         this.x = n.x - m.x;
         this.y = n.y - m.y;
     }
-    void distance(Point n , Point m){
+
+    public void distance(Point n, Point m) {
         double solve = Math.pow(m.x - n.x, 2) + Math.pow(m.y - n.y, 2);
         this.distance = Math.sqrt(solve);
 
     }
-    void display(){
+
+    void display() {
         System.out.println("x ==>" + this.x);
         System.out.println("y ==>" + this.y);
         System.out.println("distance ==> " + this.distance);
     }
-    
-    
+
 }
