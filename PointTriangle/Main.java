f6657cc00ed5755fc767efee70c6d53c4693b65a
izzasohail06 <izzasohail06@gmail.com>
2026-09-26 01:33:22 +0500

    week 05

--- PointTriangle/Main.java
+++ PointTriangle/Main.java
@@ -1,8 +1,8 @@
-public class Main{
-    public static void main(String[] args){
-        Point p1 = new Point(0,0);
-        Point p2 = new Point(8,0);
-        Point p3 = new Point(0,9);
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(8, 0);
+        Point p3 = new Point(0, 9);
 
         pointTriangle t1 = new pointTriangle(p1, p2, p3);
         t1.display();
--- PointTriangle/pointTriangle.java
+++ PointTriangle/pointTriangle.java
@@ -44,30 +44,36 @@ public class pointTriangle {
         double var1 = this.p1.distance(this.p2);
         double var3 = this.p2.distance(this.p3);
         double var5 = this.p3.distance(this.p1);
-        double var7 = (var1 + var3 + var5) / (double)2.0F;
+        double var7 = (var1 + var3 + var5) / (double) 2.0F;
         return Math.sqrt(var7 * (var7 - var1) * (var7 - var3) * (var7 - var5));
     }
 
     public boolean isRightAngle() {
-        double[] var1 = new double[]{this.p1.distance(this.p2), this.p2.distance(this.p3), this.p3.distance(this.p1)};
+        double[] var1 = new double[] { this.p1.distance(this.p2), this.p2.distance(this.p3),
+                this.p3.distance(this.p1) };
         Arrays.sort(var1);
         double var2 = var1[0];
         double var4 = var1[1];
         double var6 = var1[2];
-        return Math.pow(var2, (double)2.0F) + Math.pow(var4, (double)2.0F) == Math.pow(var6, (double)2.0F);
+        return Math.pow(var2, (double) 2.0F) + Math.pow(var4, (double) 2.0F) == Math.pow(var6, (double) 2.0F);
     }
 
     public boolean isIsosceles() {
-        return this.p1.distance(this.p2) == this.p2.distance(this.p3) || this.p2.distance(this.p3) == this.p3.distance(this.p1) || this.p3.distance(this.p1) == this.p1.distance(this.p2);
+        return this.p1.distance(this.p2) == this.p2.distance(this.p3)
+                || this.p2.distance(this.p3) == this.p3.distance(this.p1)
+                || this.p3.distance(this.p1) == this.p1.distance(this.p2);
     }
 
     public boolean isEquilateral() {
-        return this.p1.distance(this.p2) == this.p2.distance(this.p3) && this.p2.distance(this.p3) == this.p3.distance(this.p1);
+        return this.p1.distance(this.p2) == this.p2.distance(this.p3)
+                && this.p2.distance(this.p3) == this.p3.distance(this.p1);
     }
 
     public boolean isConcurrent(pointTriangle var1) {
-        double[] var2 = new double[]{this.p1.distance(this.p2), this.p2.distance(this.p3), this.p3.distance(this.p1)};
-        double[] var3 = new double[]{var1.p1.distance(var1.p2), var1.p2.distance(var1.p3), var1.p3.distance(var1.p1)};
+        double[] var2 = new double[] { this.p1.distance(this.p2), this.p2.distance(this.p3),
+                this.p3.distance(this.p1) };
+        double[] var3 = new double[] { var1.p1.distance(var1.p2), var1.p2.distance(var1.p3),
+                var1.p3.distance(var1.p1) };
         Arrays.sort(var2);
         Arrays.sort(var3);
         return var2[0] == var3[0] && var2[1] == var3[1] && var2[2] == var3[2];
